7d46902a41e4473f601e7efce0232d6ec6bcfda7
NightlyDrum <jdessyt@gmail.com>
2024-10-05 12:30:12 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const slider = document.getElementById('speed-slider');
-const output = document.getElementById('speed-output');
-
-slider.addEventListener('input', function() {
-    output.textContent = this.value;
-});
